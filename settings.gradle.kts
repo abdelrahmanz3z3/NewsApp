4591d1074204a86a04f1d74730ed0e4030f00938
Abdelrahman Mohamed <bedozaza4@gmail.com>
2023-12-25 23:44:29 +0200

    commit

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+include(":data")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://jcenter.bintray.com") }
+
+
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://jcenter.bintray.com") }
+
+    }
+}
+
+rootProject.name = "News_App"
+include(":app")
+include(":domain")
